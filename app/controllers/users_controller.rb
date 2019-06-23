1d60da1fc3b95b8504188befa3ac3dd080a5d098
yuki ueno <yuki.weaver1204@gmail.com>
2019-06-23 15:15:43 +0900

    新規登録フォーム

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,18 @@
 class UsersController < ApplicationController
   def home
   end
-  
+
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+  end
+
+  private
+    # 新規登録　ストパラ
+    def user_params
+      params.require(:user).permit(:name, :email, :password)
+    end
 end
