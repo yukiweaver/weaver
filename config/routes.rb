7f1ff5b4133578b4ae057853c04636af79d54955
yuki ueno <yuki.weaver1204@gmail.com>
2019-06-23 12:37:22 +0900

    ホーム作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  root 'application#hello'
+  # get 'users/new'
+  # root 'application#hello'
+  # get    '/basic_info/:id',  to: "users#basic_info",  as: "basic_info"   #勤怠B：基本情報の修正ページへ遷移
+  # post   '/login',   to: 'sessions#create'
+  root   'users#home'
+  resources :users
 end
