96209096067d62613661c02dedf225b02c51dcc1
yuki ueno <yuki.weaver1204@gmail.com>
2019-08-01 22:30:57 +0900

    enum日本語化

--- config/application.rb
+++ config/application.rb
@@ -10,11 +10,11 @@ module Weaver
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    　ポートフォリオ：以下の記述を追記する(設定必須)
-    config.i18n.default_locale = :ja # デフォルトのlocaleを日本語(:ja)にする
-    　#　ポートフォリオ：以下の記述を追記する(設定必須)
+    # ポートフォリオ：以下の記述を追記する(設定必須)
+    # デフォルトのlocaleを日本語(:ja)にする
+    config.i18n.default_locale = :ja
+    #ポートフォリオ：以下の記述を追記する(設定必須)
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
-
     config.time_zone = 'Tokyo'
 
     # Settings in config/environments/* take precedence over those specified here.
