7f617bdd9d9a9d04b519ef53428745e1ed52db51
yuki ueno <yuki.weaver1204@gmail.com>
2019-08-14 21:42:30 +0900

    twitter認証4

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -25,9 +25,14 @@ class SessionsController < ApplicationController
 
   def auth_create
     user = User.find_or_create_from_auth(request.env['omniauth.auth'])
-    session[:user_id] = user.uid
-    flash[:success] = "ユーザー認証が完了しました。"
-    redirect_to household_path(:user_id => user_id, :status => 'expense')
+    if user.save
+      session[:user_id] = user.id
+      flash[:success] = "ユーザー認証が完了しました。"
+      redirect_to household_path(:user_id => user_id, :status => 'expense')
+    else
+      flash[:danger] = "ユーザー認証に失敗しました。"
+      redirect_to root_path
+    end
   end
 
   # ログアウト処理
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
   has_many :incomes
   has_many :expenses
-  validates :name, presence: true, length: {in: 1..20}
+  validates :name, length: {in: 1..20}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, uniqueness: true,
+  validates :email, uniqueness: true,
                     format: {with: VALID_EMAIL_REGEX},
                     uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: {in: 6..20}
+  validates :password, length: {in: 6..20}
 
   def self.find_or_create_from_auth(auth)
     provider = auth[:provider]
