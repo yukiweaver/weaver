6d05f83ff56f762897488b7580e896ef3e75a678
yuki ueno <yuki.weaver1204@gmail.com>
2019-08-15 21:52:06 +0900

    認証バグ6

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,35 +6,49 @@ class SessionsController < ApplicationController
 
   # ログイン処理
   def create
-    user = User.find_by(email: params[:email])
-    if !user.blank?
-      user_id = user.id
-      if user.password == params[:password]
-        session[:user_id] = user_id
-        # session[:login_user] = {'_login' => {'user_id' => user.id, 'name' => user.name, 'email' => user.email}}
-        redirect_to household_path(:user_id => user_id, :status => 'expense')
+    auth = request.env['omniauth.auth']
+    if auth.present?
+      user = User.find_or_create_from_auth(request.env['omniauth.auth'])
+      session[:user_id] = user.id
+      flash[:success] = "ユーザー認証が完了しました。"
+      redirect_to household_path(:user_id => user_id, :status => 'expense')
+    else
+      user = User.find_by(email: params[:email])
+      if !user.blank?
+        user_id = user.id
+        if user.password == params[:password]
+          session[:user_id] = user_id
+          # session[:login_user] = {'_login' => {'user_id' => user.id, 'name' => user.name, 'email' => user.email}}
+          redirect_to household_path(:user_id => user_id, :status => 'expense')
+        else
+          flash.now[:danger] = 'メールアドレス、またはパスワードが間違っています。'
+          render 'new'
+        end
       else
         flash.now[:danger] = 'メールアドレス、またはパスワードが間違っています。'
         render 'new'
       end
-    else
-      flash.now[:danger] = 'メールアドレス、またはパスワードが間違っています。'
-      render 'new'
     end
   end
 
-  def auth_create
-    user = User.find_or_create_from_auth(request.env['omniauth.auth'])
-    if user
-      session[:user_id] = user.id
-      flash[:success] = "ユーザー認証が完了しました。"
-      # redirect_to household_path(:user_id => user_id, :status => 'expense')
-      redirect_to household_path(:status => 'expense')
-    else
-      flash[:danger] = "ユーザー認証に失敗しました。"
-      redirect_to root_path
-    end
-  end
+  # def auth_create
+  #   auth = request.env['omniauth.auth']
+  #   if auth.present?
+  #     user = User.find_or_create_from_auth(request.env['omniauth.auth'])
+  #     session[:user_id] = user.id
+  #     flash[:success] = "ユーザー認証が完了しました。"
+  #     redirect_to household_path(:user_id => user_id, :status => 'expense')
+  #   end
+  #   user = User.find_or_create_from_auth(request.env['omniauth.auth'])
+  #   if user
+  #     session[:user_id] = user.id
+  #     flash[:success] = "ユーザー認証が完了しました。"
+  #     redirect_to household_path(:user_id => user_id, :status => 'expense')
+  #   else
+  #     flash[:danger] = "ユーザー認証に失敗しました。"
+  #     redirect_to root_path
+  #   end
+  # end
 
   # ログアウト処理
   def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,27 @@
 class User < ApplicationRecord
   has_many :incomes
   has_many :expenses
-  validates :name, length: {in: 1..20}
+  before_save :email_downcase, unless: :uid?
+  validates :name, presence: true, unless: :uid?, length: {in: 1..20}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, uniqueness: true,
+  validates :email, presence: true, unless: :uid?, uniqueness: true,
                     format: {with: VALID_EMAIL_REGEX},
                     uniqueness: { case_sensitive: false }
-  validates :password, length: {in: 6..20}
+  validates :password, presence: true, unless: :uid?, length: {in: 6..20}
 
   def self.find_or_create_from_auth(auth)
     provider = auth[:provider]
     uid = auth[:uid]
-    user_name = auth[:info][:user_name]
+    name = auth[:info][:name]
     image_url = auth[:info][:image]
 
     self.find_or_create_by(provider: provider, uid: uid) do |user|
-      user.user_name = user_name
+      user.name = name
       user.image_url = image_url
     end
+  end
+
+  def email_downcase
+    self.email.downcase!
   end
 end
